547b4c63202d3e368762012e46a2d0ff6c9fb2c6
verfum <verfum@hotmail.co.uk>
2016-10-07 17:51:04 +0100

    Added coord and unit test

---
+++ EzEngine/Coord.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EzEngine
+{
+    public class Coord
+    {
+        public Coord(float a_x, float a_y)
+        {
+            m_x = a_x;
+            m_y = a_y;
+        }
+
+        private float m_x;
+        private float m_y;
+
+        public float X
+        {
+            set
+            {
+                m_x = value;
+            }
+            get
+            {
+                return m_x;
+            }
+        }
+
+        public float Y
+        {
+            set
+            {
+                m_y = value;
+            }
+            get
+            {
+                return m_y;
+            }
+        }
+
+    }
+}
---
+++ EzEngineTest/CoordTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EzEngine;
+
+namespace EzEngineTest
+{
+    [TestClass]
+    public class CoordTests
+    {
+        [TestMethod]
+        public void TestConstructor()
+        {
+            Coord coord = new Coord(2F, 3F);
+            Assert.AreEqual(coord.X, 2F);
+            Assert.AreEqual(coord.Y, 3F);
+        }
+    }
+}
