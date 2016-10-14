a2e1a306cb03f744cd3142bd5f1713f00d609fdd
verfum <verfum@hotmail.co.uk>
2016-10-14 17:22:44 +0100

    Update

--- EzEngine/Engine.cs
+++ EzEngine/Engine.cs
@@ -12,11 +12,21 @@ namespace EzEngine
 {
    public class Engine
    {
+      public event EzEngine.UpdateEventHandler updateEvent;
+
+
       private IDevice m_device;
 
       public Engine(IDevice a_device)
       {
          m_device = a_device;
+         a_device.updateEvent += A_device_updateEvent;
+      }
+
+      private void A_device_updateEvent(object sender, UpdateEventArgs e)
+      {
+         // Pass on unless we're going to add any params.
+         updateEvent(this, e);
       }
 
       public void run()
