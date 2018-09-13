8c354442a62c075521f5ba0741b436ab4d382bca
verfum <verfum@hotmail.co.uk>
2018-09-13 20:08:38 +0100

    add

---
+++ EzEngine/IRenderer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EzEngine
+{
+   public interface IRenderer
+   {
+      void drawImage(String a_image, Rectangle a_position);
+
+      // Property declaration:
+      Rectangle position
+      {
+         get;
+         set;
+      }
+   }
+}
